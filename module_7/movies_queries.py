2062f7decd38c5e8ac881cd523e29f7f3223b012
erihernandez365 <erihernandez@my365.bellevue.edu>
2023-11-24 23:18:51 -0600

    Update movies_queries.py

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -3,7 +3,6 @@ import mysql.connector
 try:
     # Establishing a connection to the MySQL database
     mydb = mysql.connector.connect(
-        host="DESKTOP-JPE14IK",
         user="root",
         password="ChicagoPD!311",
         database="movies"
@@ -24,7 +23,41 @@ try:
         print("Studio Name:", record[1])
         print()
 
-    # Similarly, perform operations for other queries and display data
+    # Query 2: Select genre records
+    query_genre = "SELECT Genre_ID, Genre_Name FROM Genre;"
+    cursor.execute(query_genre)
+    genre_records = cursor.fetchall()
+
+    # Displaying Genre Records
+    print("-- DISPLAYING Genre RECORDS --")
+    for record in genre_records:
+        print("Genre ID:", record[0])
+        print("Genre Name:", record[1])
+        print()
+
+    # Query 3: Select short films with runtime less than two hours
+    query_short_films = "SELECT Film_Name, Runtime FROM Films WHERE Runtime < 120;"
+    cursor.execute(query_short_films)
+    short_film_records = cursor.fetchall()
+
+    # Displaying Short Film Records
+    print("-- DISPLAYING Short Film RECORDS --")
+    for record in short_film_records:
+        print("Film Name:", record[0])
+        print("Runtime:", record[1])
+        print()
+
+    # Query 4: Select film names and directors ordered by director
+    query_director = "SELECT Film_Name, Director FROM Films ORDER BY Director;"
+    cursor.execute(query_director)
+    director_records = cursor.fetchall()
+
+    # Displaying Director of Records in Order
+    print("-- DISPLAYING Director of RECORDS in Order --")
+    for record in director_records:
+        print("Film Name:", record[0])
+        print("Director:", record[1])
+        print()
 
     # Closing the cursor and database connection
     cursor.close()
