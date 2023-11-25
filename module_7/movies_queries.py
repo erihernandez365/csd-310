3a478eecbb31e076ec6201b2bd4acae46fc83122
erihernandez365 <erihernandez@my365.bellevue.edu>
2023-11-24 23:47:59 -0600

    Update movies_queries.py

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -11,6 +11,30 @@ try:
     # Creating a cursor object using the cursor() method
     cursor = mydb.cursor()
 
+    # Query 1: Selecting studio records
+    query_studio = "SELECT studio_id, studio_name FROM studio;"
+    cursor.execute(query_studio)
+    studio_records = cursor.fetchall()
+
+    # Displaying Studio Records
+    print("-- DISPLAYING Studio Records --")
+    for record in studio_records:
+        print("Studio ID:", record[0])
+        print("Studio Name:", record[1])
+        print()
+
+    # Query 2: Selecting genre records
+    query_genre = "SELECT genre_id, genre_name FROM genre;"
+    cursor.execute(query_genre)
+    genre_records = cursor.fetchall()
+
+    # Displaying Genre Records
+    print("-- DISPLAYING Genre RECORDS --")
+    for record in genre_records:
+        print("Genre ID:", record[0])
+        print("Genre Name:", record[1])
+        print()
+
     # Query 3: Selecting short films with their runtime
     query_short_films = "SELECT film_name, film_runtime FROM film WHERE film_runtime < 120;"
     cursor.execute(query_short_films)
